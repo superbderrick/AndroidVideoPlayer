85c00ed9405cea5fdd08ac11f69dec32e39595a7
derrick <kang.derrick@gmail.com>
2017-10-07 00:18:55 +0900

    added basic project

---
+++ SummerTrimmerViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SummerTrimmerViewDemo
+//
+//  Created by Kang Jinyeoung on 07/10/2017.
+//  Copyright © 2017 SuperbDerrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
